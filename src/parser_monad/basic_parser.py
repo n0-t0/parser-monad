8fe4dbc3e2539ab84638f39730324932165f6a75
n0-t0 <ray0az0may19@gmail.com>
2024-04-06 02:05:21 +0900

    add basic_parser.py

--- src/parser_monad/basic_parser.py
+++ src/parser_monad/basic_parser.py
@@ -7,7 +7,7 @@ class BasicParser:
     def peek() -> parser.Parser[str]:
         class PeekParser(parser.Parser[str]):
             def parse(self, s: str) -> either.Either[str, tuple[str, str]]:
-                either.Either.right((s, s))
+                return either.Either.right((s, s))
         return PeekParser()
 
     @staticmethod
@@ -52,58 +52,3 @@ class BasicParser:
     @staticmethod
     def space() -> parser.Parser[str]:
         return BasicParser.satisfy(str.isspace, "expect space")
-
-    @staticmethod
-    @parser.parser_monad
-    def token[X](p: parser.Parser[X]) -> parser.Parser[X]:
-        _ = yield parser.ParserHelper.many(BasicParser.space())
-        v = yield p
-        _ = yield parser.ParserHelper.many(BasicParser.space())
-        return v
-
-    @staticmethod
-    @parser.parser_monad
-    def commaAndValue[X](p: parser.Parser[X]) -> parser.Parser[X]:
-        _ = yield BasicParser.token(BasicParser.char(","))
-        v = yield BasicParser.token(p)
-        return v
-
-    @staticmethod
-    @parser.parser_monad
-    def commaSeparated[X](p: parser.Parser[X]) -> parser.Parser[list[X]]:
-        v = yield p
-        vs = yield parser.ParserHelper.many(
-            BasicParser.commaAndValue(p)
-        )
-        return [v] + vs
-
-    @staticmethod
-    @parser.parser_monad
-    def nonEmptyList[X](p: parser.Parser[X]) -> parser.Parser[list[X]]:
-        _ = yield BasicParser.token(BasicParser.char("["))
-        v = yield BasicParser.commaSeparated(p)
-        _ = yield BasicParser.token(BasicParser.char("]"))
-        return v
-
-    @staticmethod
-    @parser.parser_monad
-    def emptyList[X]() -> parser.Parser[list[X]]:
-        _ = yield BasicParser.token(BasicParser.char("["))
-        _ = yield BasicParser.token(BasicParser.char("]"))
-        return []
-
-    @staticmethod
-    @parser.parser_monad
-    def list_literal[X](p: parser.Parser[X]) -> parser.Parser[list[X]]:
-        v = yield parser.ParserHelper.either.Either(
-            BasicParser.nonEmptyList(p), BasicParser.emptyList()
-        )
-        return v
-
-    @staticmethod
-    @parser.parser_monad
-    def between[X, Y, Z](open: parser.Parser[X], p: parser.Parser[Y], close: parser.Parser[Z]) -> parser.Parser[Y]:
-        _ = yield BasicParser.token(open)
-        v = yield p
-        _ = yield BasicParser.token(close)
-        return v
